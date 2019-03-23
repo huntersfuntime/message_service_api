497143a789ff46ca0c1568ed81f912d379d6f6b8
Hunter Orges <hunterorges@gmail.com>
2019-03-23 14:11:54 -0600

    Create a SMS tool module and bring in twillio gem

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,9 +1,12 @@
 class NotificationsController < ApplicationController
+  include SmsTool
+
   def create
     @notification = Notification.create(notification_params)
     
     respond_to do |format|
       if @notification.save
+        SmsTool.send_sms('555555555', 'A message', 'my app')
       format.json { render action: 'show', status: :created, location: @notification}
       else
         format.json { render json: @notification.errors, status: :unprocessable_entity }
